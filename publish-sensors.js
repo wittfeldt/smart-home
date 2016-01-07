6dc0c5281ffdbcc3d17bfc6d66f65dc0c7fc4c1c
Anders Wittfeldt <anders.wittfeldt@telenorconnexion.com>
2016-01-07 08:46:12 +0000

    Added DS18B20Stream.js

--- publish-sensors.js
+++ publish-sensors.js
@@ -6,8 +6,11 @@ AWS.config.credentials = new AWS.TemporaryCredentials({
 })
 
 var TellstickStream = require("./lib/tellstick-stream")
+var DS18B20Stream = require("./lib/DS18B20Stream")
 var Transforms = require("./lib/transforms")
 var Publisher = require("./lib/publisher")
+
+var mergeStream = require("merge-stream")
 
 var stdio = require('stdio');
 
@@ -15,32 +18,36 @@ var options = stdio.getopt({
 	'thingName': {
 		description: 'the thing name to publish as',
 		key: 't',
-        mandatory: true,
+		mandatory: true,
 		args: 1
 	},
 	'create': {
 		description: 'create the thing if it does not exist',
-        default: false,
+		default: false,
 		args: 1
 	},
 	'debug': {
 		description: 'print debug output',
-        key: 'd',
-        default: false
+		key: 'd',
+		default: false
 	}
 })
 
 var t = new Transforms({ 
-    highWaterMark: 16
+	highWaterMark: 16
 })
 
-var filter = t.select([ "id", "temperature", "humidity" ], true)
-var throttle59 = t.throttle(59*1000, "id")
+var filter = t.filter({ id: /[0-9]+/ }, function(chunk) {
+	return (chunk.temperature || chunk.humidity)
+})
+var select = t.select(["id", "temperature", "humidity"])
+var throttle59 = t.throttle(59*1000)
 
 new Publisher(options.thingName, options.create, function(publish) {
-    new TellstickStream("sensor")
-    .pipe(filter)     // skip irrelevant events and filter payload
-    .pipe(throttle59) // throttle readings for each sensor to ~ 1/60hz
-    .pipe(t.merge)    // merge readings for all sensors
-    .pipe(publish)    // publish to AWS IoT
+	mergeStream(new TellstickStream("sensor"), new DS18B20Stream())
+        .pipe(select)     // select fields
+	.pipe(filter)     // skip irrelevant events
+	.pipe(t.merge)    // merge readings for all sensors
+	.pipe(throttle59) // throttle to ~ 1/60hz
+	.pipe(publish)    // publish to AWS IoT
 })
