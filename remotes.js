b494c365fef40bbfb0415818fe945433911da175
Anders Wittfeldt <anders.wittfeldt@telenorconnexion.com>
2016-01-10 06:25:27 +0000

    Extracted selflearning config into remotes.js

---
+++ remotes.js
@@ -0,0 +1,32 @@
+/* Change this to match your remotes and configured tdDevices
+ * Mapping is house, unit => [ tdDevice, dimLevel ]
+*/
+
+module.exports = {
+    15112914: {
+        // Kitchen remote, page 2
+        5: [ 1, 16  ],
+        6: [ 1, 64  ],
+        7: [ 1, 128 ],
+        8: [ 1, 255 ]
+    },
+    16449394: {
+        // Livingroom remote, page 2
+        5: [ 1, 16  ],
+        6: [ 1, 64  ],
+        7: [ 1, 128 ],
+        8: [ 1, 255 ]
+    },
+
+    13781774: {
+        // On/off fob by the door
+        3: [ 1, 128 ]
+    },
+    74738: {
+        // Crappy, plastic 3 button remote
+        16: [ 1, 16 ],
+        15: [ 1, 128 ],
+        14: [ 1, 255 ]
+    }
+}
+
