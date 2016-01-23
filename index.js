c00df3d538d5d3a592717854d99fcb7e5cbd06f2
Anders Wittfeldt <anders.wittfeldt@telenorconnexion.com>
2016-01-23 21:42:49 +0100

    Republish dim commands every 30 mins for nicer graphs

--- index.js
+++ index.js
@@ -46,12 +46,15 @@ var oneWire = new DS18B20Reader()
 var events = mergeStream(tellsockEvents, oneWire)
 
 // For testing
-// var TellsockStreamRemote = require("./lib/TellsockStreamRemote")
-// var events = new TellsockStreamRemote("pi@192.168.1.161")
+var TellsockStreamRemote = require("./lib/TellsockStreamRemote")
+var events = new TellsockStreamRemote("pi@192.168.1.161")
 
 // Transforms
 var t = new Transforms({ highWaterMark: 16 })
 var dimmer = t.dim(new TelldusClient())
+var dimReplay = t.replay(30 * 60, function(obj) {
+    return (typeof obj.dimLevel != 'undefined')
+})
 
 // Sink
 var iotWriter = new IotWriter(options)
@@ -59,6 +62,7 @@ var iotWriter = new IotWriter(options)
 events
 .pipe(t.filter)            // extract and re-format relevant events
 .pipe(dimmer)              // intercept and process dim commands
+.pipe(dimReplay)           // emit last dim command once in a while for nicer graphs
 .pipe(t.prefix("id"))      // create composite payload
-.pipe(t.throttle(59*1000)) // limit to max one update per minute
-.pipe(iotWriter)           // publish
+.pipe(t.throttle(59*1000)) // limit to ~ 1/60 Hz
+.pipe(iotWriter)           // publish to AWS IoT
